ac608b1d4223638c93697d7c935e744206e9d7f6
Sindre Moen <sindrenm@gmail.com>
2018-08-07 00:22:35 +0200

    Create dark version of button

--- google_sign_in_button/lib/google_sign_in_button.dart
+++ google_sign_in_button/lib/google_sign_in_button.dart
@@ -9,9 +9,11 @@ const _packageName = 'google_sign_in_button';
 /// See https://developers.google.com/identity/branding-guidelines.
 class GoogleSignInButton extends StatelessWidget {
   final VoidCallback onPressed;
+  final Brightness brightness;
 
   GoogleSignInButton({
     @required this.onPressed,
+    this.brightness = Brightness.light,
   });
 
   @override
@@ -20,35 +22,64 @@ class GoogleSignInButton extends StatelessWidget {
       height: 40.0,
       child: Material(
         elevation: 1.0,
-        color: Colors.white,
+        color: _backgroundColor,
         child: InkWell(
           onTap: onPressed,
           splashColor: Colors.black12,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/google_logo.png',
-                  package: _packageName,
-                ),
-                SizedBox(width: 24.0),
-                Text(
-                  'Sign in with Google'.toUpperCase(),
-                  style: TextStyle(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                width: 38.0,
+                height: 38.0,
+                margin: const EdgeInsets.only(left: 1.0, top: 1.0, bottom: 1.0),
+                color: const Color(0xFFFFFFFF),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset(
+                    'assets/images/google_logo.png',
                     package: _packageName,
-                    color: Colors.black.withOpacity(0.54),
-                    fontSize: 14.0,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w500,
                   ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(width: 14.0),
+              Text(
+                'Sign in with Google'.toUpperCase(),
+                style: TextStyle(
+                  package: _packageName,
+                  color: _textColor,
+                  fontSize: 14.0,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(width: 8.0),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Color get _backgroundColor {
+    switch (brightness) {
+      case Brightness.light:
+        return const Color(0xFFFFFFFF);
+      case Brightness.dark:
+        return const Color(0xFF4285F4);
+      default:
+        throw Exception('Invalid Brightness: $brightness');
+    }
+  }
+
+  Color get _textColor {
+    switch (brightness) {
+      case Brightness.light:
+        return Colors.black.withOpacity(0.54);
+      case Brightness.dark:
+        return const Color(0xFFFFFFFF);
+      default:
+        throw Exception('Invalid Brightness: $brightness');
+    }
   }
 }
