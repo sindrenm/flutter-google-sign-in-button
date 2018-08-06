b1c36a7b3e754f983ed93fcb96664b49a942b743
Sindre Moen <sindrenm@gmail.com>
2018-08-07 00:36:01 +0200

    Toggle dark background and button style in sample

--- google_sign_in_button_sample/lib/main.dart
+++ google_sign_in_button_sample/lib/main.dart
@@ -1,28 +1,96 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in_button/google_sign_in_button.dart';
 
-void main() => runApp(new SampleApp());
+void main() => runApp(SampleApp());
 
-class SampleApp extends StatelessWidget {
+class SampleApp extends StatefulWidget {
+  @override
+  SampleAppState createState() {
+    return SampleAppState();
+  }
+}
+
+class SampleAppState extends State<SampleApp> {
+  bool _darkBackground;
+  bool _darkButtonStyle;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _darkBackground = true;
+    _darkButtonStyle = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: _themeData,
       home: Scaffold(
         body: Builder(builder: (context) {
-          return Center(
-            child: GoogleSignInButton(onPressed: () {
-              print('Pressed button!');
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GoogleSignInButton(
+                    brightness: _brightness,
+                    onPressed: () {
+                      print('Pressed button!');
 
-              const snackBar = SnackBar(
-                content: Text('Signing in …'),
-              );
+                      const snackBar = SnackBar(
+                        content: Text('Signing in …'),
+                      );
 
-              Scaffold.of(context).showSnackBar(snackBar);
-            }),
+                      Scaffold.of(context).showSnackBar(snackBar);
+                    },
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      SwitchListTile(
+                        title: Text('Dark background'),
+                        value: _darkBackground,
+                        onChanged: (value) {
+                          setState(() {
+                            _darkBackground = value;
+                          });
+                        },
+                      ),
+                      SwitchListTile(
+                        title: Text('“Dark” button style'),
+                        value: _darkButtonStyle,
+                        onChanged: (value) {
+                          setState(() {
+                            _darkButtonStyle = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           );
         }),
       ),
     );
+  }
+
+  ThemeData get _themeData {
+    if (_darkBackground) {
+      return ThemeData.dark();
+    } else {
+      return ThemeData.light();
+    }
+  }
+
+  Brightness get _brightness {
+    if (_darkButtonStyle) {
+      return Brightness.dark;
+    } else {
+      return Brightness.light;
+    }
   }
 }
